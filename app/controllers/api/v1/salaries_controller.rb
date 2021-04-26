50ee32660014b32501d5411635326e492963b6b2
kylejschulz <kylschulz@gmail.com>
2021-04-26 13:20:22 -0400

    refactor into facade and facade test

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -1,21 +1,22 @@
 class Api::V1::SalariesController < ApplicationController
   def index
-    lat_long = GeocodeService.convert_city_to_lat_long(salary_params[:location])
-    weather_data = OpenWeatherService.get_weather(lat_long[:lat], lat_long[:lng])
-    ua_id = TeleportService.get_ua_id(salary_params[:location])
-    all_salary_data =TeleportService.get_salary_info(ua_id)
-    salary_data = all_salary_data[:salaries]
-    specific_salary_data = []
-     salary_data.each do |position|
-       if ['Data Analyst', 'Data Scientist', 'Mobile Developer', 'QA Engineer', 'Software Engineer', 'Systems Administrator', 'Web Developer'].include?(position[:job][:title])
-         specific_salary_data << position
-       end
-    end
-    jobs = specific_salary_data.map do |data|
-      Job.new(data)
-    end
-    salaries = Salaries.new(salary_params[:location], weather_data, jobs)
-    render json: SalariesSerializer.new(salaries), status: 200
+    # lat_long = GeocodeService.convert_city_to_lat_long(salary_params[:location])
+    # weather_data = OpenWeatherService.get_weather(lat_long[:lat], lat_long[:lng])
+    # ua_id = TeleportService.get_ua_id(salary_params[:location])
+    # all_salary_data =TeleportService.get_salary_info(ua_id)
+    # salary_data = all_salary_data[:salaries]
+    # specific_salary_data = []
+    #  salary_data.each do |position|
+    #    if ['Data Analyst', 'Data Scientist', 'Mobile Developer', 'QA Engineer', 'Software Engineer', 'Systems Administrator', 'Web Developer'].include?(position[:job][:title])
+    #      specific_salary_data << position
+    #    end
+    # end
+    # jobs = specific_salary_data.map do |data|
+    #   Job.new(data)
+    # end
+    # salaries = Salaries.new(salary_params[:location], weather_data, jobs)
+    salaries_object = SalariesFacade.make_salary_object(salary_params[:location])
+    render json: SalariesSerializer.new(salaries_object), status: 200
   end
 
 
--- spec/api/v1/requests/salaries/salaries_spec.rb
+++ spec/api/v1/requests/salaries/salaries_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe "When i make a salary request" do
       get '/api/v1/salaries?location=denver'
       expect(response).to be_successful
       response = parse(@response)
-      require "pry"; binding.pry
+
       expect(response[:data].count).to eq(3)
       expect(response[:data].keys).to eq([:id, :type, :attributes])
       expect(response[:data][:id]).to be_nil
       expect(response[:data][:type]).to eq('salaries')
       expect(response[:data][:attributes].keys).to eq([:destination, :forecast, :salaries])
-      expect(response[:data][:attributes][:destination]).to eq("chicago")
-      expect(response[:data][:attributes][:forecast].keys).to eq([:summary, :termperature])
+      expect(response[:data][:attributes][:destination]).to eq("denver")
+      expect(response[:data][:attributes][:forecast].keys).to eq([:summary, :temperature])
       expect(response[:data][:attributes][:salaries]).to be_an(Array)
     end
   end
