2be652bb48fcb775bc41e2208257d0992d2b5f87
kylejschulz <kylschulz@gmail.com>
2021-04-26 12:03:20 -0400

    make job and salary object

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -4,11 +4,18 @@ class Api::V1::SalariesController < ApplicationController
     weather_data = OpenWeatherService.get_weather(lat_long[:lat], lat_long[:lng])
     ua_id = TeleportService.get_ua_id(salary_params[:location])
     all_salary_data =TeleportService.get_salary_info(ua_id)
-    require "pry"; binding.pry
-    salary_data = all_salary_data[:_links][:salaries]
-    specific_salary_data.map do |position|
-      position[:job][:id].include(['Data Analyst', 'Data Scientist', 'Mobile Developer', 'QA Engineer', 'Software Engineer', 'Systems Administrator', 'Web Developer'])
+    salary_data = all_salary_data[:salaries]
+    specific_salary_data = []
+     salary_data.each do |position|
+       if ['Data Analyst', 'Data Scientist', 'Mobile Developer', 'QA Engineer', 'Software Engineer', 'Systems Administrator', 'Web Developer'].include?(position[:job][:title])
+         specific_salary_data << position
+       end
     end
+    jobs = specific_salary_data.map do |data|
+      Job.new(data)
+    end
+    salary = Salary.new(destination, weather_data, jobs)
+    render json: SalarySerializer.new(salary), status: 200
   end
 
 
@@ -18,3 +25,6 @@ class Api::V1::SalariesController < ApplicationController
     params.permit(:location)
   end
 end
+
+
+# bundle exec rspec spec/api/v1/requests/salaries
